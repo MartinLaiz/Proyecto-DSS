ab379f43641e387b191b1a761069d172cb91df6f
Pablo <pgv29@alu.ua.es>
2017-02-28 18:10:50 +0100

    Creado seeder de equipo

---
+++ database/seeds/EquipoTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EquipoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Borrar datos
+        DB::('equipo')->delete();
+
+        //Insertar
+        DB::table('equipo')->insert([
+            'cif'=>'A27417476',
+            'nombre'=>'UA Football Club'
+        ]);
+    }
+}
