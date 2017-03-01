ec861191815f4bb3d95ef4e593904eb0863b3c1e
Martin Laiz Gomez <martinlaizg@gmail.com>
2017-03-01 17:04:40 +0100

    Modifica Jugador
    columna DNI en minuscula y equipo desde una select

--- database/seeds/JugadorTableSeeder.php
+++ database/seeds/JugadorTableSeeder.php
@@ -12,114 +12,113 @@ class JugadorTableSeeder extends Seeder
     public function run()
     {
          DB::table('jugador')->delete();
-
+         $equipos = DB::table('equipos')->first();
          DB::table('jugador')->insert([
-             'DNI'=> '00000000A',
+             'dni'=> '00000000A',
              'nombre' => 'Rafael',
              'apellidos' => 'Soria Diez',
              'edad' => 18,
              'posicion' => 'Delantero',
              'cargo' => 1,
-             'equipo' => 'A27417476'
+             'equipo' => $equipos->id
         ],[
-             'DNI'=> '00000001A',
+             'dni'=> '00000001A',
              'nombre' => 'Pablo',
              'apellidos' => 'Garcia Villalba',
              'edad' => 18,
              'posicion' => 'Portero',
              'cargo' => 2,
-             'equipo' => 'A27417476'
+             'equipo' => $equipos->id
         ],[
-             'DNI'=> '00000002A',
+             'dni'=> '00000002A',
              'nombre' => 'Martin',
              'apellidos' => 'Lapiz Goma',
              'edad' => 23,
              'posicion' => 'Medio',
              'cargo' => 3,
-             'equipo' => 'A27417476'
+             'equipo' => $equipos->id
         ],[
-             'DNI'=> '00000003A',
+             'dni'=> '00000003A',
              'nombre' => 'Adrian',
              'apellidos' => 'Montoya Ras',
              'edad' => 32,
              'posicion' => 'Defensa',
              'cargo' => 0,
-             'equipo' => 'A27417476'
+             'equipo' => $equipos->id
         ],[
-             'DNI'=> '00000004A',
+             'dni'=> '00000004A',
              'nombre' => 'Adrian',
              'apellidos' => 'Montoya Ras',
              'edad' => 18,
              'posicion' => 'Defensa',
              'cargo' => 0,
-             'equipo' => 'A27417476'
+             'equipo' => $equipos->id
         ],[
-             'DNI'=> '00000005A',
+             'dni'=> '00000005A',
              'nombre' => 'Manuel',
              'apellidos' => 'Alberora Melonar',
              'edad' => 23,
              'posicion' => 'Defensa',
              'cargo' => 0,
-             'equipo' => 'A27417476'
+             'equipo' => $equipos->id
         ],[
-             'DNI'=> '00000006A',
+             'dni'=> '00000006A',
              'nombre' => 'Manolo',
              'apellidos' => 'Garcia Garcia',
              'edad' => 31,
              'posicion' => 'Defensa',
              'cargo' => 0,
-             'equipo' => 'A27417476'
+             'equipo' => $equipos->id
         ],[
-             'DNI'=> '00000007A',
+             'dni'=> '00000007A',
              'nombre' => 'Higinio',
              'apellidos' => 'Garcia Garcia',
              'edad' => 20,
              'posicion' => 'Medio',
              'cargo' => 0,
-             'equipo' => 'A27417476'
+             'equipo' => $equipos->id
         ],[
-             'DNI'=> '00000008A',
+             'dni'=> '00000008A',
              'nombre' => 'Oscar',
              'apellidos' => 'Garcia Garcia',
              'edad' => 20,
              'posicion' => 'Medio',
              'cargo' => 0,
-             'equipo' => 'A27417476'
+             'equipo' => $equipos->id
         ],[
-             'DNI'=> '00000009A',
+             'dni'=> '00000009A',
              'nombre' => 'Gandalf',
              'apellidos' => 'El Gris',
              'edad' => 20,
              'posicion' => 'Medio',
              'cargo' => 0,
-             'equipo' => 'A27417476'
+             'equipo' => $equipos->id
         ],[
-             'DNI'=> '00000009A',
+             'dni'=> '00000009A',
              'nombre' => 'Harry',
              'apellidos' => 'Potter',
              'edad' => 20,
              'posicion' => 'Delantero',
              'cargo' => 0,
-             'equipo' => 'A27417476'
+             'equipo' => $equipos->id
 
         ],[
-             'DNI'=> '000000010A',
+             'dni'=> '000000010A',
              'nombre' => 'Anakin',
              'apellidos' => 'Skywalker',
              'edad' => 20,
              'posicion' => 'Delantero',
              'cargo' => 0,
-             'equipo' => 'A27417476'
+             'equipo' => $equipos->id
         ],[
-             'DNI'=> '000000011A',
+             'dni'=> '000000011A',
              'nombre' => 'Amador',
              'apellidos' => 'Rivas',
              'edad' => 20,
              'posicion' => 'Delantero',
              'cargo' => 0,
-             'equipo' => 'A27417476'
+             'equipo' => $equipos->id
 
          ]);
-        //
     }
 }
