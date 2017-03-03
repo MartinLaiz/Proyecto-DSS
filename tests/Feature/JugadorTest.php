5e5dcf080937123ed7bc845967c186de9e59d72d
Pablo <pgv29@alu.ua.es>
2017-03-03 20:35:24 +0100

    Creados tests de inserción/borrado en jugador

--- tests/Feature/JugadorTest.php
+++ tests/Feature/JugadorTest.php
@@ -16,7 +16,7 @@ class JugadorTest extends TestCase
      *
      * @return void
      */
-    public function testJugador1()
+    public function testJugador1()//Comprobación de clave ajena
     {
         $jugador = Jugador::where('apellidos','Ramos')->where('nombre','Sergio')->first();
         $equipo = Equipo::where('cif','like','%G28034718%')->first();
@@ -27,5 +27,31 @@ class JugadorTest extends TestCase
         $entrenador = Jugador::where('cargo',1)->where('apellidos','Ramos')->first();
         $equipo = Equipo::where('nombre','like','%Real Madrid%')->first();
         $this->assertEquals($equipo->id,$entrenador->equipo);
+    }
+    public function testJugador3(){//Prueba de inserción
+        $jugador = new Jugador();
+        $jugador->dni = '12345678A';
+        $jugador->nombre = 'Test';
+        $jugador->apellidos = 'Test Test';
+        $jugador->edad = 60;
+        $jugador->posicion = 'Suplente';
+        $jugador->cargo = 0;
+        $jugador->equipo = Equipo::get()->first();
+        //Lo almacenamos
+        $jugador->save();
+
+        //Lo recuperamos de la BBDD
+        $jugadorBD = Jugador::where('nombre','Test')->first();
+
+        //Comprobamos que los nombres sean iguales
+        $this->assertEquals($jugador->nombre,$jugadorBD->nombre);
+    }
+    public function testJugador4(){//Prueba de borrado de la inserción anterior
+        $jugador = Jugador::where('nombre','Test')->first();
+        $jugador->delete();
+        //Lo buscamos en la BBDD
+        $jugadorBD = Jugador::where('nombre','Test')->first();
+        //Miramos si se ha borrado
+        $this->assertEquals($jugadorBD,null);
     }
 }
