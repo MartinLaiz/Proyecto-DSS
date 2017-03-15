f20b83f6b901d891be55d2580724c37db2ab2232
Martin Laiz Gomez <martinlaizg@gmail.com>
2017-03-15 19:59:22 +0100

    Añadido Bootstrap y pueba

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+      <head>
+            <meta charset="utf-8">
+            <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
+            <title>FootballManager</title>
+      </head>
+      <body>
+            <div class="container">
+                  <button type="button" class="btn btn-primary"> Hola </button>
+            </div>
+      </body>
+</html>
