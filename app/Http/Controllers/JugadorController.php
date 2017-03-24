614498e4113c678b33966a30f0a0cfc416a77aa2
Martin Laiz Gomez <martinlaizg@gmail.com>
2017-03-24 12:57:43 +0100

    Creados Controllers

---
+++ app/Http/Controllers/JugadorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JugadorController extends Controller
+{
+    public function perfil($id){
+          Jugador::find($id);
+          return view('perfil')
+   }
+}
