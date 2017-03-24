a2363389194f3e9b14c75020382f33b040c37d4e
Martin Laiz Gomez <martinlaizg@gmail.com>
2017-03-24 11:15:05 +0100

    Arreglado seeder partido entre mismos equipos

--- database/seeds/PartidoTableSeeder.php
+++ database/seeds/PartidoTableSeeder.php
@@ -15,15 +15,17 @@ class PartidoTableSeeder extends Seeder
             $equipos = DB::table('equipo')->get();
             foreach ($equipos as $equipoLocal) {
                   foreach ($equipos as $equipoVisitante) {
-                        $fecha = mt_rand(1470052800, 1527854400);
-                        DB::table('partido')->insert([
-                              'equipoLocal'=> $equipoLocal->id,
-                              'equipoVisitante' => $equipoVisitante->id,
-                              'fecha' => date($formato, $fecha),
-                              'golesLocal' => 2,
-                              'golesVisitante' => 0,
-                              'estadio' => $equipoLocal->estadio
-                        ]);
+                        if($equipoLocal->id != $equipoVisitante->id){
+                              $fecha = mt_rand(1470052800, 1527854400);
+                              DB::table('partido')->insert([
+                                    'equipoLocal'=> $equipoLocal->id,
+                                    'equipoVisitante' => $equipoVisitante->id,
+                                    'fecha' => date($formato, $fecha),
+                                    'golesLocal' => 2,
+                                    'golesVisitante' => 0,
+                                    'estadio' => $equipoLocal->estadio
+                              ]);
+                        }
                   }
             }
 
