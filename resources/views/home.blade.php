acc90f9868dc1f56c2218b2ce75f7ddcff92f430
Martin Laiz Gomez <martinlaizg@gmail.com>
2017-03-26 12:59:13 +0200

    Dos tablas home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,56 +2,70 @@
 @section('title', 'Inicio')
 @section('content')
 
- <div class="contenedor">
-    <div class="row">
-        <div class="col-md-9">
-            @include('cabecera',array('section'=>'Inicio'))
-    <div class="row">
+<div class="contenedor">
+@include('cabecera',array('section'=>'Inicio'))
+      <div class="row">
+            <div class="col-md-6" style="padding-top:120px" >
+                  <div class="col-md-6">
+                        <table class="table table-striped">
+                              <caption><p class="text-muted">Último partido</p></caption>
+                              <thead>
+                                    <tr class="">
+                                          <th><a href="#" class="text-primary">Equipo Local</a></th>
+                                    </tr>
+                              </thead>
+                              <tbody>
+                                    <tr class="">
+                                          <td>Alineación</td>
+                                    </tr>
+                                    <tr class="">
+                                          <td>Banquillo</td>
+                                    </tr>
+                                    <tr class="">
+                                          <td>Cambios</td>
+                                    </tr>
+                                    <tr class="">
+                                          <td>Goles</td>
+                                    </tr>
+                                    <tr class="">
+                                          <td>Observaciones</td>
+                                    </tr>
+                              </tbody>
+                        </table>
+                  </div>
+                  <div class="col-md-6">
+                        <table class="table table-striped">
+                              <caption><p class="text-muted">Último partido</p></caption>
+                              <thead>
+                                    <tr class="">
+                                          <th><a href="#" class="text-primary">Equipo Visitante</a></th>
+                                    </tr>
+                              </thead>
+                              <tbody>
+                                    <tr class="">
+                                          <td>Alineación</td>
+                                    </tr>
+                                    <tr class="">
+                                          <td>Banquillo</td>
+                                    </tr>
+                                    <tr class="">
+                                          <td>Cambios</td>
+                                    </tr>
+                                    <tr class="">
+                                          <td>Goles</td>
+                                    </tr>
+                                    <tr class="">
+                                          <td>Observaciones</td>
+                                    </tr>
+                              </tbody>
+                        </table>
+                  </div>
+            </div>
 
-
-    <div class="col-md-6" style='padding-top:120px  ' ;>
-        <table class="table   ">
-            <caption><p class="text-muted">Último partido</p></caption>
-            <thead>
-                <tr class="bg-warning">
-                    <th><a href="#" class="text-primary">Equipo Local</a></th>
-                    <th><a href="#" class="text-primary">Equipo Visitante</a></th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <tr class="bg-warning">
-                     <td>Alineación</td>
-                     <td>Alineación</td>
-                </tr>
-
-                <tr class="bg-warning">
-                    <td>Banquillo</td>
-                    <td>Banquillo</td>
-                </tr>
-                <tr class="bg-warning">
-                    <td>Cambios</td>
-                    <td>Cambios</td>
-                </tr>
-                <tr class="bg-warning">
-                    <td>Goles</td>
-                    <td>Goles</td>
-                </tr>
-                <tr class="bg-warning">
-                     <td>Observaciones</td>
-                    <td>Observaciones</td>
-                </tr>
-                <tr class="bg-warning">
-                    <td>Resultado: <td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-
-     <div class='col-sm-6' style='padding-top:120px  ' >
-        <caption><p class="text-muted">Partido recientes en Liga</p></caption>
-    </div>
-</div>
+            <div class='col-sm-6' style='padding-top:120px  ' >
+                  <caption><p class="text-muted">Partido recientes en Liga</p></caption>
+            </div>
+      </div>
 
 
 @endsection
