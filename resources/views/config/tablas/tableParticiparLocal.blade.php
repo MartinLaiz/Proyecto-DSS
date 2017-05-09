b87aaf37fd4d22b0fe9f374669bbd5b53c0540a4
Unknown <rsoriadiez@gmail.com>
2017-05-09 09:27:17 +0200

    Creando modificar Participar

--- resources/views/config/tablas/tableParticiparLocal.blade.php
+++ resources/views/config/tablas/tableParticiparLocal.blade.php
@@ -1,25 +1,5 @@
-<table class="table">
     <thead>
-    <tr>
-        <th>Once Inicial</th>
-        <th>Posición</th>
-        <th>Banquillo</th>
-        <th>Posición</th>
-    </tr>
-</thead>
-    <tbody>
-        @for($i = 0; $i < 11 ; $i++)
         <tr>
-            @if($titulares[$i]->local == "si")
-                <th> {!!$titulares[$i]->usuario->nombre!!} </th>
-                @if($titulares[$i]->usuario->posicion == 1)
-                    <td>Portero</td>
-                @elseif($titulares[$i]->usuario->posicion == 2)
-                    <td>Defensa</ttdh>
-                @elseif($titulares[$i]->usuario->posicion == 3)
-                    <td>Medio</td>
-                @elseif($titulares[$i]->usuario->posicion == 4)
-                    <td>Delantero</td>
                 @else
                     <th></th>
                 @endif
