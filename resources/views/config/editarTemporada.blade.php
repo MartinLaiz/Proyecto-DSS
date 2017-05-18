a862700a0e7cea4ba34f42d1f46b280d7c19c601
Unknown <rsoriadiez@gmail.com>
2017-05-18 18:35:10 +0200

    CRUD Temporada

--- resources/views/config/editarTemporada.blade.php
+++ resources/views/config/editarTemporada.blade.php
@@ -13,8 +13,8 @@
                             <th>Nombre</th>
                             <th>Inicio</th>
                             <th>Fin</th>
+                            <th>Modificar</th>
                             <th>Borrar</th>
-
                         </tr>
                   <tbody>
 
@@ -23,9 +23,8 @@
                         <th>{!!$temporada->nombre!!}</th>
                         <th>{!! date('d/m/Y',strtotime($temporada->inicio)) !!}</th>
                         <th>{!! date('d/m/Y',strtotime($temporada->fin)) !!}</th>
-			            <th><a title="Eliminar Temporada" href="{{ action('TemporadaController@eliminarTemporada', $temporada->id)}}"  data-toggle="tooltip" type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></a></th>
-
-
+                        <th><a  data-toggle="modal" title="Modificar Temporada" href="#editarTemporada" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a></th>
+                        <th><a title="Eliminar Temporada" href="{{ action('TemporadaController@eliminarTemporada', $temporada->id)}}"  data-toggle="tooltip" type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></a></th>
                     </tr>
                     @endforeach
                   </tbody>
@@ -33,3 +32,38 @@
        </div>
  </div>
 @endsection
+
+
+{-- Temporada --}}
+<div class="modal fade" id="editarTemporada" data-target="#editarTemporada">
+      <div class="modal-dialog">
+      
+            <div class="modal-content">
+            
+                  <div class="modal-header">
+                  
+                        <button class="close" data-dismiss="modal">&times;</button>
+                  </div>
+                  
+                  <div class="modal-body">
+                        <h2> Modificar Temporada </h2>
+                          <form action = "{{ action('TemporadaController@modificarTemporda', $temporada->id)}}" method="post">
+                              {{ csrf_field() }}
+                              {{ method_field('POST') }}
+                              <div class="row form-group">
+                                    <div class="col-md-6">
+                                          <input class="form-control" onfocus="(this.type='date')" id="fecha" name="inicio" placeholder="Inicio de la temporada" type="text" required/>
+                                    </div>
+                                    <div class="col-md-6">
+                                          <input class="form-control" onfocus="(this.type='date')" id="fecha" name="fin" placeholder="Fin de la temporada" type="text" required/>
+                                    </div>
+                              </div>
+                        </div>
+                        <div class="modal-footer">
+                              <button class="btn btn-success btn-success" type="submit">Aceptar</button>
+                        </div>
+                  </form>
+            </div>
+      </div>
+</div>
+
