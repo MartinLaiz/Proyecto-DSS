432aabf9ccb29e4489622bb405aed958fbdac670
Pablo <pgv29@alu.ua.es>
2017-05-18 12:06:12 +0200

    Editar partidos v0.4

--- resources/views/config/partido/editarPartidos.blade.php
+++ resources/views/config/partido/editarPartidos.blade.php
@@ -7,17 +7,20 @@
       @include('config/configuracion')
       <div class="col-md-10 col-md-offset-1">
             <h2>Partidos</h2>
-            {{ $partidos->links() }}
+             <div class="hidden-xs">{{ $partidos->links() }}</div>
             <table class="table table-striped table-hover table-responsive" cellspacing="0" width="100%">
                   <thead>
                         <tr>
-                              <th>Equipo Local</th>
-                              <th>Equipo Visitante</th>
+                              <th class="hidden-xs">Equipo Local</th>
+                              <th class="hidden-xs">Goles Local</th>
+                              <th class="visible-xs">Local</th>
+                              <th class="visible-xs">Resultado</th>
+                              <th class="visible-xs">Visitante</th>
+                              <th class="hidden-xs">Goles Visitante</th>
+                              <th class="hidden-xs">Equipo Visitante</th>
                               <th>Estadio</th>
-                              <th>Goles Local</th>
-                              <th>Goles Visitante</th>
-                              <th>Competicion</th>
-                              <th>Temporada</th>
+                              <th class="hidden-xs">Competicion</th>
+                              <th class="hidden-xs">Temporada</th>
                               <th>Acciones</th>
                         </tr>
                   <tbody>
@@ -26,14 +29,14 @@
                   <tr onclick="window.location.href = '{{ action('ParticiparController@verParticipar', $partido->id)}}';">
                         @if($partido->temporada != null && $partido->competicion != null)
 
-                              <td><a href="{{ action('ParticiparController@verParticipar', $partido->id)}}">{!!$partido->id!!}</a></td>
                               <th>{!!$partido->equipoLocal->nombreEquipo!!}</th>
+                              <th class="hidden-xs">{!!$partido->golesLocal!!}</th>
+                              <th class="hidden-xs">{!!$partido->golesVisitante!!}</th>
+                              <th class="visible-xs">{!!$partido->golesLocal!!} - {!!$partido->golesVisitante!!}</th>
                               <th>{!!$partido->equipoVisitante->nombreEquipo!!}</th>
                               <th>{!!$partido->estadio->nombre!!}</th>
-                              <th>{!!$partido->golesLocal!!}</th>
-                              <th>{!!$partido->golesVisitante!!}</th>
-                              <th>{!!$partido->competicion->nombre!!}</th>
-                              <th>{!!$partido->temporada->nombre!!}</th>
+                              <th class="hidden-xs">{!!$partido->competicion->nombre!!}</th>
+                              <th class="hidden-xs">{!!$partido->temporada->nombre!!}</th>
                               <td>
                                     <div class="btn-group ">
                                           <a href="{{ action('PartidoController@formularioModificar', $partido->id)}}" 
@@ -52,6 +55,7 @@
                     @endforeach
                   </tbody>
             </table>
+            {{ $partidos->links() }}
        </div>
  </div>
 @endsection
