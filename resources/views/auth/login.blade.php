ba4109bd0f90e169c43e32e57f8df032ebe80286
Martin Laiz Gomez <martinlaizg@gmail.com>
2017-05-19 12:03:27 +0200

    Nuestro login funciona

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,27 @@
-@extends('layouts.app')
-
+@extends('layouts.master')
+@section('title', 'Login')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('dni') ? ' has-error' : '' }}">
-                            <label for="dni" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="dni" type="text" class="form-control" name="dni" value="{{ old('dni') }}" required autofocus>
-
-                                @if ($errors->has('dni'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('dni') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="flex-center position-ref full-height">
+      <div class="col-md-8">
+            <div>
+                  <img class="thumbnail img-responsive center-block" src="/images/Logo.png" alt="Logo FootballManager">
             </div>
-        </div>
-    </div>
+            <div>
+                  <form class="form-horizontal" role="form" action="{{ route('login') }}" method="post">
+                        {{ csrf_field() }}
+                        <div class="row col-md-9">
+                              <div class="col-md-6">
+                                    <input type="text" id="dni" type="text" class="form-control" name="dni" placeholder="Usuario">
+                              </div>
+                              <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" placeholder="Contraseña">
+                              </div>
+                        </div>
+                        <div class="col-md-3">
+                              <button type="submit" class="btn btn-success btn-block"> Iniciar sesión </button>
+                        </div>
+                  </form>
+            </div>
+      </div>
 </div>
 @endsection
